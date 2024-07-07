5a5a44163525203807e8a75da0ddc9320ecd6e60
soq1t <andrei_gruzinov@list.ru>
2024-07-07 15:21:32 +0300

    added SelectAction method to HomeworkToolkit

---
+++ HomeworkToolkit/ActionSelector.cs
@@ -0,0 +1,32 @@
+﻿namespace HomeworkToolkit
+{
+    public static class ActionSelector
+    {
+        public static Action<object> SelectAction(
+            Dictionary<KeyValuePair<ConsoleKey, string>, Action<object>> Actions
+        )
+        {
+            Console.Clear();
+            Console.WriteLine("Выберите действие:");
+
+            foreach (var item in Actions)
+            {
+                string keyName = item.Key.Key.ToString();
+                string message = item.Key.Value;
+
+                Console.WriteLine($"{keyName} - {message}");
+            }
+
+            while (true)
+            {
+                ConsoleKey pressedKey = Console.ReadKey(true).Key;
+
+                foreach (var item in Actions)
+                {
+                    if (item.Key.Key == pressedKey)
+                        return item.Value;
+                }
+            }
+        }
+    }
+}
