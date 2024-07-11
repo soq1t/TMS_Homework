464033eef0c6e93fe5e72b63e341e7f310029eda
soq1t <andrei_gruzinov@list.ru>
2024-07-11 13:01:49 +0300

    Добавил выбор действия

--- Homework6-1/Program.cs
+++ Homework6-1/Program.cs
@@ -1,30 +1,42 @@
 ﻿using Homework6_1.Doctors;
+using MyHomeworkToolkit;
 
 namespace Homework6_1
 {
     internal class Program
     {
-        private static Doctor[] _doctors;
-        private static Patient[] _patients;
+        private static List<Doctor> _doctors = new List<Doctor>();
+        private static List<Patient> _patients = new List<Patient>();
+
+        private static ActionSelector _actions = new ActionSelector();
 
         static void Main(string[] args)
         {
-            _doctors =
-            [
-                new Therapist("Светлана", 22),
-                new Surgeon("Анатолий", 41),
-                new Dantist("Ирина", 18)
-            ];
+            _doctors.Add(new Therapist("Светлана", 22));
+            _doctors.Add(new Surgeon("Анатолий", 41));
+            _doctors.Add(new Dantist("Ирина", 18));
 
-            _patients =
-            [
-                new Patient("Варфаломей", 25, "Болит ухо"),
-                new Patient("Антонина Петровна", 89, "Болит голова")
-            ];
+            _patients.Add(new Patient("Варфаломей", 25, "Болит ухо"));
+            _patients.Add(new Patient("Антонина Петровна", 89, "Болит голова"));
 
-            _patients[0].Introduce();
-            _patients[0].Heal();
-            _patients[0].CheckDiagnose();
+            _actions.AddAction("Показать список врачей", ShowDoctors);
+            _actions.AddAction("Показать список пациентов", ShowPatients);
+            _actions.AddAction("Закрыть программу", CloseProgram);
+
+            SelectAction();
         }
+
+        private static void SelectAction()
+        {
+            _actions.SelectAction();
+            Console.ReadKey(true);
+            SelectAction();
+        }
+
+        private static void CloseProgram() => Environment.Exit(0);
+
+        private static void ShowDoctors() => _doctors.ForEach(d => d.Introduce());
+
+        private static void ShowPatients() => _patients.ForEach(p => p.Introduce());
     }
 }
