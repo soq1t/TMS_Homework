3648d462259f7c6382eada9e3014a09f56fe1c44
Andrei Gruzinov <andrei_gruzinov@list.ru>
2024-07-17 14:36:35 +0300

    Добавил класс Dog

---
+++ Homework8-1/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8_1
+{
+    internal class Dog : Animal
+    {
+        public Dog(string name)
+            : base(name) { }
+    }
+}
