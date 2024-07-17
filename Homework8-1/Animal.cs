294cbbc0ae2d45e40e1be1f1ad925e87427e782f
Andrei Gruzinov <andrei_gruzinov@list.ru>
2024-07-17 14:02:56 +0300

    Создал класс Animal

---
+++ Homework8-1/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8_1
+{
+    internal abstract class Animal
+    {
+        protected string Name { get; private set; }
+
+        protected Animal(string Name) { }
+
+        public void SetName() { }
+
+        public void GetName() { }
+
+        public void Eat() { }
+    }
+}
