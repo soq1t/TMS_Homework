c5fb79b68e5a94aa154614e4b5031c32381ccc99
Andrei Gruzinov <andrei_gruzinov@list.ru>
2024-07-18 16:25:00 +0300

    Сделал ДЗ 3

---
+++ Homework8-3/Company.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyHomeworkToolkit;
+using MyHomeworkToolkit.ObjectSelecting;
+
+namespace Homework8_3
+{
+    internal class Company : ISelectableObject
+    {
+        public string CompanyName { get; private set; }
+
+        private Dictionary<int, Employee> _employers;
+        private int _employeeId = 0;
+
+        public int? EmployersAmount => _employers?.Count;
+
+        public string DisplayedName => $"Компания \"{CompanyName}\"";
+
+        public void PrintInfo()
+        {
+            ConsoleUtility.WriteColored("Название компании: ", ConsoleColor.Cyan);
+            ConsoleUtility.WriteLineColored(CompanyName, ConsoleColor.Green);
+        }
+
+        public Company(string companyName)
+        {
+            CompanyName = companyName;
+            _employers = new Dictionary<int, Employee>();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            if (!HasEmployee(employee))
+                _employers.Add(_employeeId++, employee);
+        }
+
+        public void PrintEmployers()
+        {
+            PrintInfo();
+            ConsoleUtility.WriteColored("Кол-во сотрудников: ", ConsoleColor.Cyan);
+            ConsoleUtility.WriteLineColored(_employers.Count.ToString(), ConsoleColor.Green);
+
+            foreach (var e in _employers)
+            {
+                Console.WriteLine();
+                ConsoleUtility.WriteColored("ID сотрудника: ", ConsoleColor.Cyan);
+                ConsoleUtility.WriteLineColored(e.Key.ToString(), ConsoleColor.Green);
+                e.Value.PrintInfo();
+            }
+        }
+
+        public bool HasEmployee(Employee employee) => _employers.Values.Contains(employee);
+    }
+}
--- Homework8-3/Program.cs
+++ Homework8-3/Program.cs
@@ -1,10 +1,103 @@
-﻿namespace Homework8_3
+﻿using MyHomeworkToolkit;
+using MyHomeworkToolkit.ObjectSelecting;
+
+namespace Homework8_3
 {
     internal class Program
     {
+        private static ActionSelector _actions;
+        private static ActionSelector _companiesActions;
+
+        private static List<Company> _companies;
+        private static Company _selectedCompany;
+
+        static Program()
+        {
+            _companies = new List<Company>()
+            {
+                { new Company("Google") },
+                { new Company("Yandex") },
+            };
+
+            Company active = _companies.First();
+            active.AddEmployee(new Director("John", active));
+            active.AddEmployee(new Worker("Bill", active));
+            active.AddEmployee(new Worker("Soul", active));
+            active.AddEmployee(new Worker("Jack", active));
+            active.AddEmployee(new Accountant("Nelly", active));
+
+            active = _companies.Last();
+            active.AddEmployee(new Director("Иван", active));
+            active.AddEmployee(new Worker("Сергей", active));
+            active.AddEmployee(new Worker("Василиса", active));
+            active.AddEmployee(new Worker("Анатолий", active));
+            active.AddEmployee(new Accountant("Светлана", active));
+
+            _actions = new ActionSelector();
+            _actions.AddAction("Выбор компании", SelectCompany);
+            _actions.AddSeparator();
+            _actions.AddExitProgramAction();
+
+            _companiesActions = new ActionSelector();
+
+            _companiesActions.AddAction("Показать работников компании", PrintEmployers);
+            _companiesActions.AddSeparator();
+            _companiesActions.AddAction(
+                "Вернуться на главную",
+                () =>
+                    _actions.SelectActionRepeated(MainMessage, pressKeyAfterActionCompleted: false)
+            );
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            _actions.SelectActionRepeated(MainMessage, pressKeyAfterActionCompleted: false);
+        }
+
+        private static void MainMessage()
+        {
+            ConsoleUtility.WriteColored(
+                "Количество зарегистрированных компаний: ",
+                ConsoleColor.Cyan
+            );
+            ConsoleUtility.WriteLineColored(_companies.Count.ToString(), ConsoleColor.Yellow);
+
+            int employersAmount = 0;
+
+            foreach (Company company in _companies)
+                employersAmount += company.EmployersAmount ?? 0;
+
+            ConsoleUtility.WriteColored(
+                "Суммарное количество работников в компании: ",
+                ConsoleColor.Cyan
+            );
+            ConsoleUtility.WriteLineColored(employersAmount.ToString(), ConsoleColor.Yellow);
+            Console.WriteLine();
+            ConsoleUtility.WriteLineColored("Выберите желаемое действие:", ConsoleColor.Yellow);
+        }
+
+        private static void CompaniesMessage()
+        {
+            _selectedCompany?.PrintInfo();
+            ConsoleUtility.WriteLineColored("Выберите желаемое действие:", ConsoleColor.Yellow);
+        }
+
+        private static void SelectCompany()
+        {
+            _selectedCompany = ObjectSelector<Company>.SelectFromList(
+                _companies,
+                "Выберите компанию из списка ниже:"
+            );
+
+            if (_selectedCompany != null)
+            {
+                _companiesActions.SelectAction(CompaniesMessage);
+            }
+        }
+
+        private static void PrintEmployers()
+        {
+            _selectedCompany.PrintEmployers();
         }
     }
 }
