3cae13d41461115bc16db159cfa0a699471cbb3f
Andrei Gruzinov <andrei_gruzinov@list.ru>
2024-07-26 20:39:29 +0300

    Создал класс Debt, реализовал методы, поля и свойства

--- Homework9-1/Program.cs
+++ Homework9-1/Program.cs
@@ -2,9 +2,6 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        static void Main(string[] args) { }
     }
 }
