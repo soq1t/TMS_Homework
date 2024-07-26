636dfc1496595891cec5f3ac966b8c92fc8df466
Andrei Gruzinov <andrei_gruzinov@list.ru>
2024-07-26 21:34:25 +0300

    Создал интерфейс IVehicle

---
+++ Homework9-3/IVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9_3
+{
+    internal interface IVehicle
+    {
+        void Drive();
+
+        bool Refuel(int fuelAmount);
+    }
+}
