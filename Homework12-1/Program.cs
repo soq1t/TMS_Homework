33dd88ad711d4201d046319047f885b66990092b
soq1t <andrei_gruzinov@list.ru>
2024-08-03 17:10:07 +0300

    Lesson 12, Homework 1

---
+++ Homework12-1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework12_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
