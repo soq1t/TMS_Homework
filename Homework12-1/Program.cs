5e75dbabe39cebef8f63fc03e7c0b67c430bbfb6
soq1t <andrei_gruzinov@list.ru>
2024-08-03 17:25:40 +0300

    made homework

--- Homework12-1/Program.cs
+++ Homework12-1/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Homework12_1
+﻿using MyHomeworkToolkit;
+
+namespace Homework12_1
 {
     internal class Program
     {
+        private static readonly ActionSelector _actions;
+
+        static Program()
+        {
+            _actions = new ActionSelector();
+            _actions.AddAction("Регистрация", Register);
+            _actions.AddSeparator();
+            _actions.AddExitProgramAction();
+        }
+
+        private static void Register()
+        {
+            string login = InputDataHandler.GetTextData("Введите логин");
+            string password = InputDataHandler.GetTextData("Введите пароль");
+            string passwordConfirm = InputDataHandler.GetTextData("Повторите пароль");
+
+            Authentification.LogIn(login, password, passwordConfirm);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            _actions.SelectActionRepeated();
         }
     }
 }
---
+++ Homework12-1/WrongLoginException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework12_1
+{
+    internal class WrongLoginException : Exception
+    {
+        public WrongLoginException()
+            : base() { }
+
+        public WrongLoginException(string message)
+            : base(message) { }
+    }
+}
