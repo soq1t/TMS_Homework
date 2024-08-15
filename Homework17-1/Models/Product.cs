e394b93544a102b168cf919661310c6c57bdcec3
soq1t <andrei_gruzinov@list.ru>
2024-08-16 01:22:02 +0300

    added api controller

---
+++ Homework17-1/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Homework17_1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+        public int Amount { get; set; }
+
+        public decimal Price { get; set; }
+
+        public void Update(Product product)
+        {
+            foreach (
+                PropertyInfo prop in typeof(Product).GetProperties().Where(p => p.Name != "Id")
+            )
+            {
+                prop.SetValue(this, prop.GetValue(product));
+            }
+        }
+    }
+}
